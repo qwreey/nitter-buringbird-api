7ef28ac687e4f2e6b9491020231165e0ce6d9226
Qwreey <dev@qwreey.kr>
2023-07-17 03:08:54 +0900

    Add rss api

--- src/v1/handle.ts
+++ src/v1/handle.ts
@@ -3,6 +3,7 @@ import {
 } from "../index"
 import { Type } from '@sinclair/typebox'
 import NitterBuringbirdAPI from "./api"
+import RSS from "rss"
 
 export default function(instance: instance, _options:any, done:VoidFunction) {
     const api = new NitterBuringbirdAPI()
@@ -24,6 +25,65 @@ export default function(instance: instance, _options:any, done:VoidFunction) {
         } as const,
         async handler(request, reply) {
             reply.send(await api.getBuringbirdResult())
+        }
+    })
+
+    instance.get("/v1/rss",{
+        schema: {
+            params: {
+                
+            }
+        },
+        async handler(request, reply) {
+            let now = new Date()
+            const newFeed = new RSS({
+                title: "불타는 짹짹이 추적기",
+                description: "트위터가 불타고 있는지 추적합니다",
+                feed_url: "https://burningbird.qwreey.kr/v1/rss",
+                site_url: "https://burningbird.qwreey.kr",
+                pubDate: now,
+            })
+            let apiResult = await api.getBuringbirdResult()
+
+            newFeed.item({
+                title: `30 분동안 ${apiResult.countLast30Mins} 트윗`,
+                description: "",
+                url: "",
+                guid: "1",
+                categories: ["none"],
+                author: "Qwreey",
+                date: now
+            })
+            newFeed.item({
+                title: `20 분동안 ${apiResult.countLast20Mins} 트윗`,
+                description: "",
+                url: "",
+                guid: "1",
+                categories: ["none"],
+                author: "Qwreey",
+                date: now
+            })
+            newFeed.item({
+                title: `10 분동안 ${apiResult.countLast10Mins} 트윗`,
+                description: "",
+                url: "",
+                guid: "1",
+                categories: ["none"],
+                author: "Qwreey",
+                date: now
+            })
+            newFeed.item({
+                title: `5 분동안 ${apiResult.countLast5Mins} 트윗`,
+                description: "",
+                url: "",
+                guid: "1",
+                categories: ["none"],
+                author: "Qwreey",
+                date: now
+            })
+
+            reply.type("application/xml")
+            reply.send(newFeed.xml())
         }
     })
 
